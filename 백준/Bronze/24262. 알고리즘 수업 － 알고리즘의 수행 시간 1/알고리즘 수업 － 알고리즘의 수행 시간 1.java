1e7cfacf48af99d2044ac95c0daf46821865b928
hs_1991317_YoonseokYang <115922214+hs-1991317-YoonseokYang@users.noreply.github.com>
2024-06-06 23:26:54 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 216 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner in= new Scanner(System.in);
+        int a=in.nextInt();
+        System.out.println(1);
+        System.out.print(0);
+    }
+}
